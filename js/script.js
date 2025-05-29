b81c03bd062f0bfd95151b7c5b494e7cfc99dfec
Maria <eduarda25.mecd@gmail.com>
2025-05-29 20:32:44 -0300

    Mais uma mudança

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+  const toggleBtn = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  toggleBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("open");
+  });
+
